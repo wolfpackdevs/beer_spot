17a0992eda1a79fa77eaa421ddfece26414f9bd8
Yonibrese <yonibrese@gmail.com>
2020-01-08 13:11:22 +0200

    finished tutorial program

--- finder/urls.py
+++ finder/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('dashbord/', views.dashbord, name='dashbord'),
+    path('tutorial/', views.tutorial_1, name='tutorial_1'),
+    path('tutorial/<int:id>', views.tutorial_2, name='tutorial_2'),
 ]
--- finder/views.py
+++ finder/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+
 from accounts.models import Brewer, Viewer
+from brewer.models import GenericBeer, Beer
 
 
 # Create your views here.
@@ -10,3 +12,22 @@ def dashbord(request):
     if Brewer.objects.filter(user=request.user).exists():
         return redirect('brewer')
     return render(request, 'finder/dashbord.html')
+
+
+@login_required
+def tutorial_1(request):
+    if Brewer.objects.filter(user=request.user).exists():
+        return redirect('brewer')
+    g_beer = GenericBeer.objects.all()
+    return render(request, 'finder/tutorial_1.html', {'g_beer': g_beer})
+
+
+@login_required
+def tutorial_2(request, id):
+    if Brewer.objects.filter(user=request.user).exists():
+        return redirect('brewer')
+    g_beer = GenericBeer.objects.get(id=id)
+    beers_s = Beer.objects.filter(style=g_beer.style)
+    beers_f = Beer.objects.filter(flavor__in=g_beer.flavor.all())
+    return render(request, 'finder/tutorial_2.html', {'beers_s': beers_s,
+                                                      'beers_f': beers_f})
