0ab0d39373b17eff82323365a0ec6334b1b99783
Yonibrese <yonibrese@gmail.com>
2020-01-30 08:53:03 +0200

    before heroku

--- Beer_finder/settings.py
+++ Beer_finder/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'brewer',
     'accounts',
     'finder',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -125,10 +126,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 LOGIN_REDIRECT_URL = 'dashbord'
 LOGOUT_REDIRECT_URL = '/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+# AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+# AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+# AWS_STORAGE_BUCKET_NAME = 'beer-finder-django'
+#
+# AWS_S3_FILE_OVERWRITE = False
+# AWS_DEFAULT_ACL = None
+#
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
