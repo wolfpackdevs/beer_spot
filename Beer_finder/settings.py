b827e3240e3cab6c1ff530f11d75da662ea6bbd0
jonathan <yonibrese@gmail.com>
2020-04-13 09:43:08 +0300

    removed comment

--- Beer_finder/settings.py
+++ Beer_finder/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#hello there
 SECRET_KEY = os.environ.get('SK_django')
 
 # SECURITY WARNING: don't run with debug turned on in production!
