aa209038ec7f9eb230650d6a2e01d7e347e914e2
jonathan <yonibrese@gmail.com>
2020-05-10 09:55:22 +0300

    sending email with gmail for pass reset

--- Beer_finder/settings.py
+++ Beer_finder/settings.py
@@ -137,6 +137,13 @@ LOGOUT_REDIRECT_URL = '/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# e-mail backend for development
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # only for development
+# e-mail backend for development with console
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+# email with gmail
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
